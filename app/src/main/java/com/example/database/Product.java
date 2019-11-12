68e59f1d5573fb8b65cb92bd76cedd41dc79ff1e
CFA19SCM95L <56765241+CFA19SCM95L@users.noreply.github.com>
2019-11-12 15:50:10 -0600

    init

---
+++ app/src/main/java/com/example/database/Product.java
@@ -0,0 +1,41 @@
+package com.example.database;
+
+public class Product {
+
+    private String product_ID;
+    private String product_name;
+    private String category;
+    private String price;
+
+    public String getProduct_ID() {
+        return product_ID;
+    }
+
+    public void setProduct_ID(String product_ID) {
+        this.product_ID = product_ID;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
